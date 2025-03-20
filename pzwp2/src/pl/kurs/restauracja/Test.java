ef906ecca17ec59d7d796dc3605d460a590abc05
RafaL <zielony.banan88@gmail.com>
2025-03-20 18:32:20 +0100

    v1.0.1

--- pzwp2/src/pl/kurs/restauracja/Test.java
+++ pzwp2/src/pl/kurs/restauracja/Test.java
@@ -1,23 +1,24 @@
 package pl.kurs.restauracja;
 
 public class Test {
-public static void main(String[] args) {
-	przygotujZupe();
-}
-public static void przygotujZupe() {
-	
-	Zupa zupa = new Zur();
-	Kucharz k = new Kucharz();
-	Marchewka marchewka = new Marchewka();
-	k.skladniki.add(marchewka);
-	
-	Ryz ryz = new Ryz();
-	k.skladniki.add(ryz);	
-	
-	k.przygotuj(zupa);
-	Podawacz ke = new ZupaNaWynos();
-	if(ke instanceof Adresowalny)
-		((Adresowalny)ke).setAdres("Katowice");
-	ke.podaj(zupa);
-}
+    public static void main(String[] args) {
+        przygotujZupe();
+    }
+
+    public static void przygotujZupe() {
+
+        Zupa zupa = new Zur();
+        Kucharz k = new Kucharz();
+        Marchewka marchewka = new Marchewka();
+        k.skladniki.add(marchewka);
+
+        Ryz ryz = new Ryz();
+        k.skladniki.add(ryz);
+
+        k.przygotuj(zupa);
+        Podawacz ke = new ZupaNaWynos();
+        if (ke instanceof Adresowalny)
+            ((Adresowalny) ke).setAdres("Katowic");
+        ke.podaj(zupa);
+    }
 }
